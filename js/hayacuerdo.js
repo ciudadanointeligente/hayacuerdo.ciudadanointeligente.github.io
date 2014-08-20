af09dc57be1dda583edd8ca2413a43b48db96620
José Damián Garrido Muñoz <damian.garrido.munoz@gmail.com>
2014-08-20 16:17:50 -0400

    add view more

--- js/hayacuerdo.js
+++ js/hayacuerdo.js
@@ -17,27 +17,43 @@ $('#listado').on('change', 'select#lol', function(){
 
                       var posture1 = '',
                           posture_img = '',
-                          posture2 = '';
+                          posture2 = '',
+                          view_more_a = '',
+                          view_more_b = '',
+                          modal_posture_a = '',
+                          modal_posture_b = '';
 
+                      view_more_a = '<br><i class="fa fa-plus"></i> <a data-toggle="modal" data-target="#myModal'+index+'a">ver más</a>';
+                      view_more_b = '<br><i class="fa fa-plus"></i> <a data-toggle="modal" data-target="#myModal'+index+'b">ver más</a>';
+                      
                       if( row.class === 'noacuerdo' ) {
-                        posture1 = '<div class="col-md-2 noacuerdo postura1"><p>'+row.postura1+posture1_twitter+'</p></div>';
+                        posture1 = '<div class="col-md-2 noacuerdo postura1"><p>'+row.postura1+view_more_a+posture1_twitter+'</p></div>';
                         posture_img = '<div class="col-md-2 hidden-xs noizquierda"><img class="izq" src="img/noacuerdoizq.svg" alt="lala"></div><div class="col-md-2 col-md-offset-1 text-center texto"><i class="hidden-md hidden-lg fa fa-times fa-5x"></i></div><div class="col-md-2 col-md-offset-1 hidden-xs text-right noderecha"><img class="der" src="img/noacuerdoder.svg" alt="lala"></div>';
-                        posture2 = '<div class="col-md-2 noacuerdo postura2"><p>'+row.postura2+posture2_twitter+'</p></div>';
+                        posture2 = '<div class="col-md-2 noacuerdo postura2"><p>'+row.postura2+view_more_b+posture2_twitter+'</p></div>';
+
+                        modal_posture_a = '<div class="modal fade" id="myModal'+index+'a" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+row.postura1+'</h4></div><div class="modal-body">'+row.postura1txtlargo+'</div><div class="modal-footer"></div></div></div></div>';
+                        modal_posture_b = '<div class="modal fade" id="myModal'+index+'b" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+row.postura2+'</h4></div><div class="modal-body">'+row.postura2txtlargo+'</div><div class="modal-footer"></div></div></div></div>';
                       }
 
                       if( row.class === 'acuerdoparcial' ) {
-                        posture1 = '<div class="col-md-2 col-md-offset-1 acuerdoparcial postura1"><p>'+row.postura1+posture1_twitter+'</p></div>';
+                        posture1 = '<div class="col-md-2 col-md-offset-1 acuerdoparcial postura1"><p>'+row.postura1+view_more_a+posture1_twitter+'</p></div>';
                         posture_img = '<div class="col-md-2 hidden-xs parcialderecha"><img class="izq" src="img/acuerdoparcializq.svg" alt="lala"></div><div class="col-md-2 text-center texto"><i class="hidden-sm hidden-md hidden-lg fa fa-exclamation fa-5x"></i></div><div class="col-md-2 hidden-xs text-right parcializquierda"><img class="der" src="img/acuerdoparcialder.svg" alt="lala"></div>';
-                        posture2 = '<div class="col-md-2 acuerdoparcial postura2"><p>'+row.postura2+posture2_twitter+'</p></div>';
+                        posture2 = '<div class="col-md-2 acuerdoparcial postura2"><p>'+row.postura2+view_more_b+posture2_twitter+'</p></div>';
+
+                        modal_posture_a = '<div class="modal fade" id="myModal'+index+'a" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+row.postura1+'</h4></div><div class="modal-body">'+row.postura1txtlargo+'</div><div class="modal-footer"></div></div></div></div>';
+                        modal_posture_b = '<div class="modal fade" id="myModal'+index+'b" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+row.postura2+'</h4></div><div class="modal-body">'+row.postura2txtlargo+'</div><div class="modal-footer"></div></div></div></div>';
                       }
 
                       if( row.class === 'acuerdototal' ) {
-                        posture1 = '<div class="col-md-2 col-md-offset-3 acuerdototal postura1"><p>'+row.postura1+posture1_twitter+'</p></div>';
+                        posture1 = '<div class="col-md-2 col-md-offset-3 acuerdototal postura1"><p>'+row.postura1+view_more_a+posture1_twitter+'</p></div>';
                         posture_img = '<div class="col-md-2 total text-center"><img class="izq hidden-xs hidden-sm" src="img/acuerdototal.svg" alt="lala"><i class="col-md-2 hidden-sm hidden-md hidden-lg fa fa-check fa-5x"></i></div>';
-                        posture2 = '<div class="col-md-2 acuerdototal postura2"><p>'+row.postura2+posture2_twitter+'</p></div>';
+                        posture2 = '<div class="col-md-2 acuerdototal postura2"><p>'+row.postura2+view_more_b+posture2_twitter+'</p></div>';
+
+                        modal_posture_a = '<div class="modal fade" id="myModal'+index+'a" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+row.postura1+'</h4></div><div class="modal-body">'+row.postura1txtlargo+'</div><div class="modal-footer"></div></div></div></div>';
+                        modal_posture_b = '<div class="modal fade" id="myModal'+index+'b" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+row.postura2+'</h4></div><div class="modal-body">'+row.postura2txtlargo+'</div><div class="modal-footer"></div></div></div></div>';
                       }
 
-                      var positionContainer = "<div class='row'>"+posture1+posture_img+posture2+"</div>";
+                      var positionContainer = "<div class='row'>"+posture1+posture_img+posture2+modal_posture_a+modal_posture_b+"</div>";
 
                       allPositions.append(positionContainer)
                     })
@@ -57,7 +73,7 @@ function showInfo(data, tabletop) {
 
   list.appendTo('#listado');
 
-  sheet = 'gobierno-oposicion';
+  var sheet = 'gobierno-fech';
 
   
 	$.each(tabletop.sheets(sheet).all(), function(index, row){
@@ -67,27 +83,41 @@ function showInfo(data, tabletop) {
 
 		var posture1 = '',
         posture_img = '',
-        posture2 = '';
+        posture2 = '',
+        view_more_a = '',
+        view_more_b = '';
 
+    view_more_a = '<br><i class="fa fa-plus"></i> <a data-toggle="modal" data-target="#myModal'+index+'a">ver más</a>';
+    view_more_b = '<br><i class="fa fa-plus"></i> <a data-toggle="modal" data-target="#myModal'+index+'b">ver más</a>';
+    
     if( row.class === 'noacuerdo' ) {
-      posture1 = '<div class="col-md-2 noacuerdo postura1"><p>'+row.postura1+posture1_twitter+'</p></div>';
+      posture1 = '<div class="col-md-2 noacuerdo postura1"><p>'+row.postura1+view_more_a+posture1_twitter+'</p></div>';
       posture_img = '<div class="col-md-2 hidden-xs noizquierda"><img class="izq" src="img/noacuerdoizq.svg" alt="lala"></div><div class="col-md-2 col-md-offset-1 text-center texto"><i class="hidden-md hidden-lg fa fa-times fa-5x"></i></div><div class="col-md-2 col-md-offset-1 hidden-xs text-right noderecha"><img class="der" src="img/noacuerdoder.svg" alt="lala"></div>';
-      posture2 = '<div class="col-md-2 noacuerdo postura2"><p>'+row.postura2+posture2_twitter+'</p></div>';
+      posture2 = '<div class="col-md-2 noacuerdo postura2"><p>'+row.postura2+view_more_b+posture2_twitter+'</p></div>';
+      
+      modal_posture_a = '<div class="modal fade" id="myModal'+index+'a" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+row.postura1+'</h4></div><div class="modal-body">'+row.postura1txtlargo+'</div><div class="modal-footer"></div></div></div></div>';
+      modal_posture_b = '<div class="modal fade" id="myModal'+index+'b" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+row.postura2+'</h4></div><div class="modal-body">'+row.postura2txtlargo+'</div><div class="modal-footer"></div></div></div></div>';
     }
 
     if( row.class === 'acuerdoparcial' ) {
-      posture1 = '<div class="col-md-2 col-md-offset-1 acuerdoparcial postura1"><p>'+row.postura1+posture1_twitter+'</p></div>';
+      posture1 = '<div class="col-md-2 col-md-offset-1 acuerdoparcial postura1"><p>'+row.postura1+view_more_a+posture1_twitter+'</p></div>';
       posture_img = '<div class="col-md-2 hidden-xs parcialderecha"><img class="izq" src="img/acuerdoparcializq.svg" alt="lala"></div><div class="col-md-2 text-center texto"><i class="hidden-sm hidden-md hidden-lg fa fa-exclamation fa-5x"></i></div><div class="col-md-2 hidden-xs text-right parcializquierda"><img class="der" src="img/acuerdoparcialder.svg" alt="lala"></div>';
-      posture2 = '<div class="col-md-2 acuerdoparcial postura2"><p>'+row.postura2+posture2_twitter+'</p></div>';
+      posture2 = '<div class="col-md-2 acuerdoparcial postura2"><p>'+row.postura2+view_more_b+posture2_twitter+'</p></div>';
+
+      modal_posture_a = '<div class="modal fade" id="myModal'+index+'a" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+row.postura1+'</h4></div><div class="modal-body">'+row.postura1txtlargo+'</div><div class="modal-footer"></div></div></div></div>';
+      modal_posture_b = '<div class="modal fade" id="myModal'+index+'b" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+row.postura2+'</h4></div><div class="modal-body">'+row.postura2txtlargo+'</div><div class="modal-footer"></div></div></div></div>';
     }
 
     if( row.class === 'acuerdototal' ) {
-      posture1 = '<div class="col-md-2 col-md-offset-3 acuerdototal postura1"><p>'+row.postura1+posture1_twitter+'</p></div>';
+      posture1 = '<div class="col-md-2 col-md-offset-3 acuerdototal postura1"><p>'+row.postura1+view_more_a+posture1_twitter+'</p></div>';
       posture_img = '<div class="col-md-2 total text-center"><img class="izq hidden-xs hidden-sm" src="img/acuerdototal.svg" alt="lala"><i class="col-md-2 hidden-sm hidden-md hidden-lg fa fa-check fa-5x"></i></div>';
-      posture2 = '<div class="col-md-2 acuerdototal postura2"><p>'+row.postura2+posture2_twitter+'</p></div>';
+      posture2 = '<div class="col-md-2 acuerdototal postura2"><p>'+row.postura2+view_more_b+posture2_twitter+'</p></div>';
+
+      modal_posture_a = '<div class="modal fade" id="myModal'+index+'a" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+row.postura1+'</h4></div><div class="modal-body">'+row.postura1txtlargo+'</div><div class="modal-footer"></div></div></div></div>';
+      modal_posture_b = '<div class="modal fade" id="myModal'+index+'b" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4 class="modal-title" id="myModalLabel">'+row.postura2+'</h4></div><div class="modal-body">'+row.postura2txtlargo+'</div><div class="modal-footer"></div></div></div></div>';
     }
 
-		var positionContainer = "<div class='row'>"+posture1+posture_img+posture2+"</div>";
+		var positionContainer = "<div class='row'>"+posture1+posture_img+posture2+modal_posture_a+modal_posture_b+"</div>";
 
 		allPositions.append(positionContainer)
   })
